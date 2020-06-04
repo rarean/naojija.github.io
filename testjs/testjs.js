bd194f5c7ecedd5be1b2f255927bd6202511e408
Nao Jija <65327370+naojija@users.noreply.github.com>
2020-06-03 20:37:22 -0700

    Delete testjs.js

--- testjs/testjs.js
+++
@@ -1,9 +0,0 @@
-function order() {
-    const message = document.querySelector("#message");
-    const checkedInput = document.querySelector("input:checked");
-    if (checkedInput) {
-      message.textContent = `You have placed an order for ${checkedInput.value}.`;
-    } else {
-      message.textContent - "Unknown order.";
-    }
-  }
